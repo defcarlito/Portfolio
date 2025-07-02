654d71eefd8247fd17622f7816c563c2f2e71a39
Harrison Amorim <harrisoncamorim@gmail.com>
2025-07-02 10:39:06 -0500

    reorganized file structure

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import { VStack, HStack, Flex, Image, Icon, Heading, Text, Separator } from "@chakra-ui/react"
+import { MapPin } from 'lucide-react'
+
+function Profile() {
+    return (
+        <VStack>
+            <Flex dir="row" alignItems="center" gap={6} p={6}>
+                <VStack>
+                    <Image 
+                        rounded="full" 
+                        boxSize="150px" 
+                        src="https://bit.ly/dan-abramov" 
+                        alt="Dan Abramov" 
+                    />
+                </VStack>
+                <VStack align="start" gap={0}>
+                    <Heading size="2xl">hey, i'm Harrison</Heading>
+                    <Text color="gray.400">studying CS @ University of St. Thomas</Text>
+                    <Separator borderColor="transparent" p={1}/>
+                    <HStack color="gray.400">
+                        <Icon size="sm">
+                            <MapPin />
+                        </Icon>
+                        <Text fontSize="sm">Saint Paul, MN</Text>
+                    </HStack>
+                </VStack>
+            </Flex>
+        </VStack>
+    )
+}
+
+export default Profile
---
+++ src/components/sections/top/IconButtonRow.jsx
@@ -0,0 +1,49 @@
+import { Box, Flex, HStack, Button, IconButton } from "@chakra-ui/react"
+import { Linkedin, Github, Mail, FileDown } from 'lucide-react'
+
+function IconButtonRow() {
+    return (
+            <HStack justifyContent="space-between">
+                <Flex 
+                    dir="row"
+                    bg="white"
+                    border="1px solid"
+                    borderColor="gray.200" 
+                    shadow="0 0 5px rgba(0, 0, 0, 0.1)"
+                    borderRadius="lg"
+                    w="fit-content"
+                    my={2}
+                    p={1}
+                >   
+                    <Box>
+                        <IconButton color="gray.600" bg="white" _hover={{ bg: "gray.100" }} size="sm">
+                            <Linkedin />
+                        </IconButton>
+                        <IconButton color="gray.600" bg="white" _hover={{ bg: "gray.100" }} size="sm">
+                            <Github />
+                        </IconButton>
+                        <IconButton color="gray.600" bg="white" _hover={{ bg: "gray.100" }} size="sm">
+                            <Mail />
+                        </IconButton>
+                    </Box>
+                </Flex>
+                <Flex
+                    dir="row"
+                    bg="white"
+                    border="1px solid"
+                    borderColor="gray.200" 
+                    shadow="0 0 5px rgba(0, 0, 0, 0.1)"
+                    borderRadius="lg"
+                    w="fit-content"
+                    my={2}
+                    p={1}
+                >
+                    <Button color="gray.600" bg="white" _hover={{ bg: "gray.100" }} size="sm">
+                        <FileDown /> Resume
+                    </Button>
+                </Flex>
+            </HStack>
+    )
+}
+
+export default IconButtonRow
